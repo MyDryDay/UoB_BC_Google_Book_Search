49391df2a9ec519300b8c88bd9c6a3d2bbb1f710
George Cope <G.Cope13@gmail.com>
2021-05-19 16:03:26 +0100

    Created server file to connect to a mongoDB database called 'googlebooks', will need to come back and require routes once they are complete

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// *** REQUIRE ROUTES HERE ONCE COMPLETE ***
+
+// Middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// Static assets
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+}
+
+// *** UNCOMMENT ROUTES ONCE COMPLETED ***
+// app.use(routes);
+
+// Connect to DB
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks',
+    {
+        useCreateIndex: true,
+        useNewUrlParser: true
+    }
+);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+});
