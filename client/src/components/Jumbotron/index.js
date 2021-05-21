9a0895873f7beced015b4673f406a2c1ff6aa541
George Cope <G.Cope13@gmail.com>
2021-05-21 16:15:53 +0100

    Added a header and link to Google Books in jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -7,7 +7,8 @@ const Jumbotron = () => {
                 <div className='jumbotron'>
                     <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
                         <div className='h-100 p-5 m-0 text-white bg-dark'>
-                    
+                            <h1>Search & Save Books Using Google's Book API</h1>
+                            <a href='http://www.google.co.uk/books/'>Google Books</a>
                         </div>
                     </div>
                 </div>
