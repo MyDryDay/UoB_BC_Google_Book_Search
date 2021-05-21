80691deb914dd3bdd6423394980cf184933d09d1
George Cope <G.Cope13@gmail.com>
2021-05-21 16:16:39 +0100

    Created folder called pages and added a 'search' page with the Jumbotron rendered

---
+++ client/src/pages/Search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Jumbotron from '../components/Jumbotron/index';
+
+const Search = () => {
+    return (
+        <Jumbotron />
+    )
+}
+
+export default Search;
