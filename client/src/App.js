1bfd1fb8c307df61766a2806591df2bb05e267cc
George Cope <G.Cope13@gmail.com>
2021-05-21 14:19:09 +0100

    imports & including Navbar in return statement

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
 import './App.css';
 
 function App() {
   return (
-    
+    <Router>
+      <div>
+        <Navbar />
+      </div>
+    </Router>
   );
 }
 
