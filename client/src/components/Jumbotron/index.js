a900c09e695b4af5d8d7c55e85de7fa5902086a9
George Cope <G.Cope13@gmail.com>
2021-05-21 14:25:57 +0100

    Created bootstrap5 equivalent of a jumbotron

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Jumbotron = () => {
+    return (
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='jumbotron'>
+                    <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+                        <div className='h-100 p-5 m-0 text-white bg-dark'>
+                    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
