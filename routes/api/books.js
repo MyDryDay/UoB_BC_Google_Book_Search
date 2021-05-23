2f517770b8d645c5fea69bfabed33313171cc9b1
George Cope <G.Cope13@gmail.com>
2021-05-23 15:06:38 +0100

    Created get/post/delete routes for database & index file for bookRoutes

---
+++ routes/api/books.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const booksController = require('../../controllers/booksController');
+
+router.route('/')
+    .get(
+        booksController.findAll
+    )
+    .post(
+        booksController.create
+    );
+
+router.route('/:id')
+    .get(
+        booksController.findbyId
+    )
+    .delete(
+        booksController.remove
+    );
+
+module.exports = router;
