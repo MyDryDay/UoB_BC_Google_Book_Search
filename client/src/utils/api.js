fa44ce5f2b276881e7dc4b463d43606fb751e94b
George Cope <G.Cope13@gmail.com>
2021-05-23 15:08:47 +0100

    Created utils folder containing the api.js file, this contains all the databse functions for searching, getting saving & deleting books

---
+++ client/src/utils/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const baseURL = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+export default {
+    // Get all books
+    searchBook: function (q) {
+        return axios.get(baseURL + q);
+    },
+    getBooks: function () {
+        return axios.get('/api/books');
+    },
+    // Delete a book
+    deleteBook: function (id) {
+        return axios.delete('/api/books' + id)
+            .then(response => response.data);
+    },
+    // Save a book
+    saveBook: function (bookData) {
+        return axios.post('/api/books', bookData);
+    },
+    // Gets a book
+    getBook: function (id) {
+        return axios.get('/api/books' + id);
+    }
+};
