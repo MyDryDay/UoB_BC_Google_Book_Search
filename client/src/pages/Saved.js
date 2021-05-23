692197cc1a1933378bb3054476327589bc758179
George Cope <G.Cope13@gmail.com>
2021-05-23 19:07:11 +0100

    Created the Saved page, logic for deleting a book does not work though

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Jumbotron from '../components/Jumbotron/index';
-import Button from '../components/Button/index';
 import { Booklist, BookItem } from '../components/BookComponents/index';
 
 import API from '../utils/api';
 
 const Saved = () => {
+    const [saved, setSaved] = useState([]);
+
+    useEffect(() => {
+        loadBooks()
+    }, [])
+
+    const loadBooks = () => {
+        API.getBooks('/api/books')
+            .then(
+                res => setSaved(res.data)
+            )
+            .catch(
+                err => console.log(err)
+            );
+    }
+
+    const handleDelete = (id) => {
+        API.deleteBook(id)
+            .then(
+                (res) => {
+                    loadBooks();
+                }
+            )
+            .catch(
+                err => console.log(err)
+            );
+    }
+
     return (
-        
+        <div>
+            <Jumbotron />
+            <Booklist>
+                {saved.map(book => (
+                    <BookItem
+                        key={book._id}
+                        title={book.title}
+                        authors={book.authors.join(', ')}
+                        description={book.description}
+                        link={book.link}
+                        image={book.image}
+                        Button={() => (
+                            <button 
+                                onClick={() => handleDelete(book._id)}
+                                className='btn btn-danger'>
+                            Delete
+                            </button>
+                        )}
+                    />
+                ))}
+            </Booklist>
+        </div>
     )
 }
 
