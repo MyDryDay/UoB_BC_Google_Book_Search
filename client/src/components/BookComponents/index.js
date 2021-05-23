c25de14f40807691c3a5f968f5eb6f75f17595db
George Cope <G.Cope13@gmail.com>
2021-05-23 15:12:20 +0100

    Created a <ul> component that accepts children & a <li> component that accepts the props of the books

---
+++ client/src/components/BookComponents/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export const Booklist = ({children}) => {
+    return (
+        <ul className='list-group'>
+            {children}
+        </ul>
+    )
+}
+
+export const BookItem = ({
+    title,
+    authors,
+    description,
+    image,
+    link,
+    Button
+}) => {
+    return (
+        <li className='list-group-item'>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-xs-4 col-sm-3'>
+                        <img src={image || 'https://via.placeholder.com/100'} alt={title}></img>
+                    </div>
+                    <div className='col-xs-8 col-sm-9'>
+                        <h4>{title} by {authors}</h4>
+                        <p>{description}</p>
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <Button />
+                    <a rel='noreferrer noopener' target='_blank' href={link}>
+                    <button className='btn'>View Book</button></a>
+                </div>
+            </div>
+        </li>
+    )
+}
