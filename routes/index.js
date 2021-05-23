7d65970111a6153770445a981df02cdc698666b2
George Cope <G.Cope13@gmail.com>
2021-05-23 15:07:18 +0100

    Created api index.js file

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const path = require('path');
+
+router.use('./api', apiRoutes);
+
+router.use((req, res) => {
+    // CHANGE TO CLIENTS/BUILD/INDEX.HTML BEFORE DEPLOYMENT
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
+module.exports = router;
