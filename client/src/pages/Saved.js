428ebcb3d84dd851a9c1b5b9e76449909ffe1844
George Cope <G.Cope13@gmail.com>
2021-05-23 18:49:40 +0100

    Created template for Saved page, including imports

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Jumbotron from '../components/Jumbotron/index';
+import Button from '../components/Button/index';
+import { Booklist, BookItem } from '../components/BookComponents/index';
+
+import API from '../utils/api';
+
+const Saved = () => {
+    return (
+        
+    )
+}
+
+export default Saved;
